f9e1bee9206e9a213d62ed4784b537fc28ad46b6
盧奕叡 <yirueilu.b@gmail.com>
2020-09-19 09:54:25 +0800

    Git push script

--- git_push.sh
+++ git_push.sh
@@ -4,7 +4,20 @@ git add .
 
 echo "Please add commit message"
 read message
-git commit -m "${message}"
 
-echo "Push to develop branch"
-git push -u origin develop
+while true
+do
+    echo "Are you sure to commit with ${message} message? ( y/n )"
+    read confirm
+    if [[ "${confirm}" == "y" ]]; then
+        git commit -m "${message}"
+        echo "Push to develop branch"
+        git push -u origin develop
+        break
+    elif [[ "${confirm}" == "n" ]]; then
+        echo "Not commit and push"
+        break
+    else
+        echo "Please enter 'y' or 'n'"
+    fi
+done
